6102727f365aeb2686b229024bc49c20e32ef5f0
cherry-aggarwal <125724703+cherry-aggarwal@users.noreply.github.com>
2025-08-26 02:59:06 +0530

    Update libr.rb

--- Casks/libr.rb
+++ Casks/libr.rb
@@ -1,5 +1,5 @@
 cask "libr" do
-  version "1.0.0"
+  version "1.1.0"
   sha256 "d710c4612702660afe6c7f94f0467f45d1898f74363e5dcbc37f2909bd5d67b4"
 
   url "https://github.com/cherry-aggarwal/LIBR/releases/download/v#{version}/libr.dmg"
